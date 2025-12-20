148bdda1a1df6de7671d90991ecd1d1d09ac26c7
Soiisa <133542752+Soiisa@users.noreply.github.com>
2025-12-20 21:50:08 +0000

    Add files via upload

--- pages/api/servers/provision.js
+++ pages/api/servers/provision.js
@@ -958,7 +958,7 @@ async function provisionServer(serverRow, version, ssh_keys, res) {
     const payload = {
       name: serverRow.name,
       server_type: serverType,
-      image: '342669034', // CUSTOM SNAPSHOT NAME
+      image: '342669261', // CUSTOM SNAPSHOT NAME
       user_data: sanitizedUserData,
       ssh_keys: sshKeysToUse,
       location: 'nbg1',
