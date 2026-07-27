68c128dae13b6db7647a91a69d7d6d2de20ffa56
Soiisa <133542752+Soiisa@users.noreply.github.com>
2026-07-27 21:10:38 +0100

    Add files via upload

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://spawnly.net',
-  generateRobotsTxt: true, // Automatically generates robots.txt
-  exclude: ['/server/*', '/admin/*','/dashboard'], // Don't let Google index private user panels
+  generateRobotsTxt: true,
+  // This removes these paths from the sitemap.xml
+  exclude: ['/server/*', '/admin/*', '/dashboard', '/server-sitemap.xml'], 
+  
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        // This tells Google NOT to crawl these private paths
+        disallow: ['/server/', '/admin/', '/dashboard/'], 
+      },
+    ],
+    additionalSitemaps: [
+      `${process.env.SITE_URL || 'https://spawnly.net'}/server-sitemap.xml`,
+    ],
+  },
 }
